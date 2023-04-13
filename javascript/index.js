7f8024bbf2cb0082e727e9cb94772f9c54cff99b
cbautista452 <123851261+cbautista452@users.noreply.github.com>
2023-04-13 18:03:59 -0400

    updated changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,5 @@
-alert("hello!");
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.quereyselector(".date");
+let losAngelesTimeselement = losAngelesElement.querySelector(".time");
+losAngelesElement.innerHTML = moment().format("MMMM Do yyyy");
+losAngelesTimeElement.innerHTML = "1:49:15<small>AM</small>!";
